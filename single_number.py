7d1f081108125059e3fc457b03e0ce233aff7eb8
Koat <kessentini.youssef03@gmail.com>
2025-08-21 17:49:00 +0100

    e

---
+++ single_number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: int) -> int:
+        checker = {}
+        for i in range (0,len(nums)) :
+            if nums[i] not in checker :
+                checker.update({nums[i] : 1})
+            else : 
+                checker[nums[i]] += 1
+        
+        value_to_find = 1
+        for key , value in checker.items() :
+            if value == value_to_find :
+                return key
+
