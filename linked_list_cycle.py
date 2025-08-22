26a67949f62ce7719412be00acf55bee59364b3c
Koat <kessentini.youssef03@gmail.com>
2025-08-22 17:57:43 +0100

    corrected problem

---
+++ linked_list_cycle.py
@@ -0,0 +1,24 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:        
+        
+        checker = {}
+        while head != None :
+            
+            if (head in checker) and (checker[head] == 1) :
+                return True
+            
+            elif  head not in checker :
+                checker.update({head : 1})
+           
+            else :
+                checker[head] += 1
+            
+            head = head.next
+
+        return False
+    
