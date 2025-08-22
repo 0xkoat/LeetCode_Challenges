ad66441d06b4d9073bf4a8d73aed057540bd474c
Koat <kessentini.youssef03@gmail.com>
2025-08-22 11:42:07 +0100

    corrected broblem

--- single_number.py
+++ single_number.py
@@ -1,13 +1,12 @@
 class Solution:
     def singleNumber(self, nums: int) -> int:
-        checker = {}
-        for i in range (0,len(nums)) :
-            if nums[i] not in checker :
-                checker.update({nums[i] : 1})
-            else : 
-                checker[nums[i]] += 1
         
-        value_to_find = 1
-        for key , value in checker.items() :
-            if value == value_to_find :
-                return key
+        result = nums[0]
+        for num in nums[1:len(nums)] :
+            result = result^num
+
+        return result
+        
+        
+
+     
