325de0124c1f655446f99bdf50a264db5690d9eb
Koat <kessentini.youssef03@gmail.com>
2025-08-23 00:45:22 +0100

    Spent more than 5 hours doing this

---
+++ intersection_of_two_linked_lists.py
@@ -0,0 +1,78 @@
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+
+        pointerA = headA
+        pointerB = headB
+  
+        while pointerA != pointerB :
+
+            if pointerA == None :
+                pointerA = headB
+            else :   
+                pointerA = pointerA.next
+            
+            if pointerB == None :
+                pointerB = headA
+            else :      
+                pointerB = pointerB.next
+        
+        return pointerA
+
+        
+            
+
+            
+       
+            
+       
+
+
+        
+
+
+
+
+               
+       
+
+
+       
+       
+       
+       
+           
+        
+          
+           
+            
+        
+       
+       
+       
+
+
+           
+           
+       
+
+
+        
+ 
+
+
+
+
+        
+    
+        
+        
+
+
+
+
