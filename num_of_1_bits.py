f1e1894550317b0c446ba1d33dd1694f0ff2681d
Koat <kessentini.youssef03@gmail.com>
2025-08-24 18:12:03 +0100

    ez also

---
+++ num_of_1_bits.py
@@ -0,0 +1,15 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        power = 0
+        while 2**(power)<= n :
+            power += 1   
+        power -= 1
+
+        counter = 0
+        sum = 0
+        for i in range (power , -1 , -1) :
+            if (sum + 2**i) <= n :
+                sum += 2**i
+                counter += 1
+         
+        return counter
