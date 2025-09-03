831195956d6918a39c3bca8b9868e35d7c78b015
Koat <kessentini.youssef03@gmail.com>
2025-09-03 13:03:10 +0100

    happy now?

--- num_of_1_bits.py
+++ num_of_1_bits.py
@@ -1,11 +1,9 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        a = bin(n)[2:]
         counter = 0
-        for i in range (0,len(a)) :
-            if ((int(a[i])) & (int(a[i]))) == 1 :
-                counter += 1
+        while n :
+            counter += n & 1
+            n >>= 1
         return counter
- 
      
         
